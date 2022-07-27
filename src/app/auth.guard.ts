a6de592ba3a2079d755b5bb297c51a5761573a1d
rbhvsr <vishva.padariya@confidosoft.co.uk>
2022-07-27 19:07:19 +0530

    Initial commit

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router} from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -7,11 +7,9 @@ import { AuthService } from './auth.service';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService : AuthService , private  route : Router){}
+  constructor(private authService: AuthService, private route: Router) { }
 
-  canActivate()
- {
-  return true;
-}
-
+  canActivate() {
+    return true;
+  }
 }
